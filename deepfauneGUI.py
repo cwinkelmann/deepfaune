fdb9061482fc2cbdf3641b869b55d52e7af9c61f
Vincent Miele <vincent.miele@univ-lyon1.fr>
2021-10-05 09:54:52 +0200

    New model with 20 classes

--- deepfauneGUI.py
+++ deepfauneGUI.py
@@ -25,8 +25,9 @@ sg.ChangeLookAndFeel('DarkGrey1')
 backbone = "efficientnet"
 batch_size = 16
 workers = 1
-hdf5 = "efficientnet11spVide.hdf5"
-classes = ["blaireau","cerf","chamois","chevreuil","chien","ecureuil","lagomorphe","loup","mustelide","renard","sanglier","vide"]
+hdf5 = "efficientnet.hdf5"
+classes = ["blaireau","cerf","chamois","chevreuil","chien","ecureuil","felinae","humain","lagomorphe","loup","micromammifere","mouflon","mouton","mustelide","oiseau","renard","sanglier","vache","vehicule","vide"]
+
 
 ### GUI WINDOW
 prediction = [[],[]]
