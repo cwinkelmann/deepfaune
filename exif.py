fcee43499435ba86d4d4a20d5fc5a3a8ded65774
Vincent Miele <vincent.miele@univ-lyon1.fr>
2022-01-10 15:34:54 +0100

    Majority voting working in test. Needs integration in GUI

--- exif.py
+++ exif.py
@@ -15,47 +15,64 @@ nbfiles = df_filename.shape[0]
 
 
 predictedclass = ['test' for i in range(nbfiles)]
+import random
+predictedscore = [random.random() for i in range(nbfiles)]
 
 
-## Getting date from exif, or draw random fake date
 from PIL import Image
 def get_date_taken(path):
    try:
-       date = Image.open(path)._getexif()[36867]
+      date = Image.open(path)._getexif()[36867]
    except:
-       date = None
-   return(date)
+      date = None
+   return date
 
 import random
 from time import time
+from datetime import datetime
 def randomDate(seed):
     random.seed(seed)
     d = random.randint(1, int(time()))
     return datetime.fromtimestamp(d).strftime("%Y:%m:%d %H:%M:%S")
+ 
+   
+def correctPredictionWithSequence():
+   ## Getting date from exif, or draw random fake date
+   dates = np.array([get_date_taken(file) for file in df_filename["filename"]])
+   withoutdate = np.where(dates == None)[0]
+   dates[withoutdate] = [randomDate(int(i)) for i in withoutdate]
+   
+   ## Sorting dates and computing lag
+   from datetime import timedelta
+   datesstrip =  np.array([datetime.strptime(date, "%Y:%m:%d %H:%M:%S") for date in dates])
+   datesorder = np.argsort(datesstrip)
+   datesstripSorted = np.sort(datesstrip)
+   
+   def majorityVotingInSequence(i1, i2):
+      df = pd.DataFrame({'prediction':[predictedclass[k] for k in datesorder[i1:(i2+1)]], 'score':[predictedscore[k] for k in datesorder[i1:(i2+1)]]})
+      majority = df.groupby(['prediction']).sum()
+      if list(majority.index) == ['vide']:
+         pass # only empty images
+      else:
+         majority = majority[majority.index != 'vide'] # skipping empty images in sequence
+         best = np.argmax(majority['score']) # selecting class with best total score
+         majorityclass = majority.index[best]
+         majorityscore = df.groupby(['prediction']).mean()['score'][best] # overall score as the mean for this class
+         predictedclass[i1:(i2+1)] = np.repeat(majorityclass, i2-i1+1)
+         predictedscore[i1:(i2+1)] = np.repeat(majorityscore, i2-i1+1)
+      
+   ## Treating sequences
+   i1 = i2 = 0 # sequences boundaries
+   for i in range(1,len(datesstripSorted)):
+      lag = datesstripSorted[i]-datesstripSorted[i-1]
+      if lag<timedelta(seconds=20): # subsequent images in sequence
+         pass
+      else: # sequence change
+         print("treating sequence ",i1,i2)
+         majorityVotingInSequence(i1, i2)
+         i1 = i
+      i2 = i
+   print("treating sequence ",i1,i2)
+   majorityVotingInSequence(i1, i2)
+   return predictedclass, predictedscore
 
-dates = np.array([get_date_taken(file) for file in df_filename["filename"]])
-withoutdate = np.where(dates == None)[0]
-dates[withoutdate] = [randomDate(int(i)) for i in withoutdate]
-
-## Sorting dates and computing lag
-from datetime import datetime, timedelta
-datesstrip =  np.array([datetime.strptime(date, "%Y:%m:%d %H:%M:%S") for date in dates])
-datesorder = np.argsort(datesstrip)
-lagv =  datesstripSorted[2:len(datesstripSorted)] - datesstripSorted[1:(len(datesstripSorted)-1)]
-np.where(lagv<timedelta(seconds=20))[0]
-
-## Treating sequences
-i1 = i2 = 0 # sequences boundaries
-for i in range(1,len(datesstripSorted)):
-   lag = datesstripSorted[i]-datesstripSorted[i-1]
-   if lag<timedelta(seconds=20): # subsequent images in sequence
-      pass
-   else: # sequence change
-      print("treating sequence ",i1,i2)
-      print(datesstrip[datesorder[i1]],datesstrip[datesorder[i2]])
-      i1 = i
-   i2 = i
-print("treating sequence ",i1,i2)   
-
-def predictionWithSequence(i1, i2):
-   [predictedclass[k] for k in datesorder[i1:(i2+1)]]
