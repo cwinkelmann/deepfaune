7fa4992910d48df1766054e51fe238aaa5c443a8
Elias Chetouane <chetouane.elias@gmail.com>
2022-03-07 17:01:36 +0100

    Adding comments to video2frame.py

--- devtest/video2frame.py
+++ devtest/video2frame.py
@@ -1,24 +1,23 @@
 import cv2
 import os
 
-folder = input("Nom du dossier : ")
-video_name = input("Nom du fichier : ")
+folder = input("Nom du dossier : ") # the folder where the frames will be extracted into
+video_name = input("Nom du fichier : ") # the video we want to extract frames from
 vidcap = cv2.VideoCapture(video_name)
-frames = []
+frames = [] # array of all extracted frames (not optimal to do so, but easier to understand for testing)
 
 while True:
-    success,image = vidcap.read()
-    if not success:
+    success,image = vidcap.read() # extracts a frame (1 by 1)
+    if not success: # if not success, there is no more frames to extract
         break
-    frames.append(image)
+    frames.append(image) # adding the frame into the array (not optimal)
 
 size = len(frames)
-step = int(size/10)
-
+step = int(size/10) # to keep 1 frame every 10 frames
 if step == 0:
 	step == 1
 
 for i in range(0, size, step):    
-    cv2.imwrite(os.path.join(folder,"{}_frame{:d}.jpg".format(video_name.split(".")[-2],i)), frames[i])
+    cv2.imwrite(os.path.join(folder,"{}_frame{:d}.jpg".format(video_name.split(".")[-2],i)), frames[i]) # creates a jpg file for the frame we extracted
 
 print("{} images are extracted from {}, {} are saved.".format(i,folder, int(size/step)))
