92f810c6a985dab30163cdd26699d4d29e63a4c5
Vincent Miele <vincent.miele@univ-lyon1.fr>
2022-04-28 12:10:13 +0200

    Changing detection to relative boxes in [0,1] then back to pixels.

--- detectTools.py
+++ detectTools.py
@@ -32,51 +32,24 @@
 # knowledge of the CeCILL license and that you accept its terms.
 
 ####################################################################################
-### LOADING YOLO 
+### LOADING YOLO
 ####################################################################################
-import tensorflow as tf
+import cv2
 import numpy as np
 
-savedmodel = "checkpoints/yolov4-608/"
-saved_model_loaded = tf.saved_model.load(savedmodel)
-infer = saved_model_loaded.signatures['serving_default']
-
-def detecting(batch_data, CROP_SIZE):
-    ## INFERING boxes and retain the most confident one (if it exists)
-    pred_bbox = infer(input_1=batch_data) # dans yolo
-    for key, value in pred_bbox.items():
-        boxes = value[:, :, 0:4]
-        pred_conf = value[:, :, 4:]
-    if boxes.shape[1]>0: # not empty
-        boxes, scores, classes, valid_detections = tf.image.combined_non_max_suppression(
-            boxes=tf.reshape(boxes, (tf.shape(boxes)[0], -1, 1, 4)),
-            scores=tf.reshape(
-                pred_conf, (tf.shape(pred_conf)[0], -1, tf.shape(pred_conf)[-1])),
-            max_output_size_per_class=5,
-            max_total_size=5,
-            iou_threshold=0.45,
-            score_threshold=0.25
-        )
-        idxmax  = np.unravel_index(np.argmax(scores.numpy()[0,:]), scores.shape[1])
-        ## CROPPING a single box
-        NUM_BOXES = 1 # boxes.numpy().shape[1]
-        box_indices = tf.random.uniform(shape=(NUM_BOXES,), minval=0, maxval=1, dtype=tf.int32)
-        output = tf.image.crop_and_resize(batch_data, boxes[0,idxmax[0]:(idxmax[0]+1),:], box_indices, (CROP_SIZE, CROP_SIZE))
-        output.shape
-        return output, True
-    return [], False
-
-
-import cv2
 YOLO_SIZE=608
 model = 'my-yolov4_last.weights'
 config = 'my-yolov4.cfg'
 yolo = cv2.dnn.readNetFromDarknet(config, model)
 yololayers = [yolo.getLayerNames()[i - 1] for i in yolo.getUnconnectedOutLayers()]
 
-
-# in/out as numpy int array (0-255) in BGR
+####################################################################################
+### BEST BOX DETECTION 
+####################################################################################
 def bestBoxDetection(image, threshold=0.25):
+    '''
+    in/out as numpy int array (0-255) in BGR
+    '''
     height, width = image.shape[:2]
     # here resizing and scaling by 1./255 + swapBR since OpenCV uses BGR
     blobimage = cv2.dnn.blobFromImage(image, 1/255.0, (YOLO_SIZE, YOLO_SIZE), swapRB=True, crop=False)
@@ -84,7 +57,6 @@ def bestBoxDetection(image, threshold=0.25):
     layerOutputs = yolo.forward(yololayers)
     boxes_detected = []
     confidences_scores = []
-    probability_index=5
     for output in layerOutputs:
         # Looping over each of the detections
         for detection in output:
@@ -92,12 +64,11 @@ def bestBoxDetection(image, threshold=0.25):
             boxclass = np.argmax(scores)
             confidence = scores[boxclass]        
             if confidence > threshold:
-                # Bounding box in full size
-                box = detection[0:4] * np.array([width, height, width, height])
-                (boxcenterx, boxcentery, boxwidth, boxheight) = box.astype("int")
+                # Bounding box in [0,1]x[0,1]
+                (boxcenterx, boxcentery, boxwidth, boxheight) = detection[0:4]
                 # Use the center (x, y)-coordinates to derive the top and left corner of the bounding box
-                cornerx = int(boxcenterx - (boxwidth / 2))
-                cornery = int(boxcentery - (boxheight / 2))
+                cornerx = (boxcenterx - (boxwidth / 2))
+                cornery = (boxcentery - (boxheight / 2))
                 boxes_detected.append([cornerx, cornery, boxwidth, boxheight])
                 confidences_scores.append(float(confidence))
     # Removing overlap and duplicates
@@ -105,9 +76,13 @@ def bestBoxDetection(image, threshold=0.25):
     if final_boxes.size>0:
         # Extract the most confident bounding box coordinates
         best_box = final_boxes[0]
-        (cornerx, cornery) = (boxes_detected[best_box][0], boxes_detected[best_box][1])
+        (cornerx, cornery) = (boxes_detected[best_box][0], boxes_detected[best_box][1])        
         (boxwidth, boxheight) = (boxes_detected[best_box][2], boxes_detected[best_box][3])
-        print((cornerx, cornery),(cornerx+boxwidth, cornery+boxheight))
+        # Back to image dimension in pixels
+        cornerx = np.around(cornerx*width).astype("int")
+        boxwidth = np.around(boxwidth*width).astype("int")
+        cornery = np.around(cornery*height).astype("int")
+        boxheight = np.around(boxheight*height).astype("int")
         croppedimage = image[cornery:(cornery+boxheight), cornerx:(cornerx+boxwidth)]
         return croppedimage, True
     return [], False
