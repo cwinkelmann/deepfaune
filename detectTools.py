c2735375a6a5c9c63f8f4efa07cfe521d52553f9
Elias Chetouane <chetouane.elias@gmail.com>
2022-06-03 14:24:24 +0200

    Adding DetectorJSON class to make the detector work with Megadetector

--- detectTools.py
+++ detectTools.py
@@ -93,3 +93,111 @@ class Detector:
             return croppedimage, True
         return [], False
 
+
+
+####################################################################################
+
+import json
+import os
+from typing import Dict, Mapping, Optional, Tuple
+
+import pandas as pd
+
+# Source : https://github.com/microsoft/CameraTraps/blob/main/api/batch_processing/postprocessing/load_api_results.py
+
+def load_api_results(api_output_path: str, normalize_paths: bool = True,
+                     filename_replacements: Optional[Mapping[str, str]] = None
+                     ) -> Tuple[pd.DataFrame, Dict]:
+    """
+    Loads the json formatted results from the batch processing API to a
+    Pandas DataFrame, mainly useful for various postprocessing functions.
+    Args:
+        api_output_path: path to the API output json file
+        normalize_paths: whether to apply os.path.normpath to the 'file' field
+            in each image entry in the output file
+        filename_replacements: replace some path tokens to match local paths to
+            the original blob structure
+    Returns:
+        detection_results: pd.DataFrame, contains at least the columns:
+                ['file', 'max_detection_conf', 'detections','failure']            
+        other_fields: a dict containing fields in the dict
+    """
+    #print('Loading API results from {}'.format(api_output_path))
+
+    with open(api_output_path) as f:
+        detection_results = json.load(f)
+
+    #print('De-serializing API results')
+
+    # Sanity-check that this is really a detector output file
+    for s in ['info', 'detection_categories', 'images']:
+        assert s in detection_results, 'Missing field {} in detection results'.format(s)
+
+    # Fields in the API output json other than 'images'
+    other_fields = {}
+    for k, v in detection_results.items():
+        if k != 'images':
+            other_fields[k] = v
+
+    # Normalize paths to simplify comparisons later
+    if normalize_paths:
+        for image in detection_results['images']:
+            image['file'] = os.path.normpath(image['file'])
+            # image['file'] = image['file'].replace('\\','/')
+
+    # Pack the json output into a Pandas DataFrame
+    detection_results = pd.DataFrame(detection_results['images'])
+
+    # Replace some path tokens to match local paths to original blob structure
+    # string_to_replace = list(filename_replacements.keys())[0]
+    if filename_replacements is not None:
+        for string_to_replace in filename_replacements:
+
+            replacement_string = filename_replacements[string_to_replace]
+
+            for i_row in range(len(detection_results)):
+                row = detection_results.iloc[i_row]
+                fn = row['file']
+                fn = fn.replace(string_to_replace, replacement_string)
+                detection_results.at[i_row, 'file'] = fn
+
+    #print('Finished loading and de-serializing API results for {} images from {}'.format(
+            #len(detection_results),api_output_path))
+
+    return detection_results, other_fields
+
+class DetectorJSON:
+    
+    def __init__(self, jsonfilename):
+        # getting results in a dataframe
+        self.df_json, df_notUsed = load_api_results(jsonfilename)
+        self.k = 0
+        
+    def nextBestBoxDetection(self):
+        image_path = str(self.df_results["file"][self.k])
+        image = cv2.imread(image_path)
+        if image is None:
+            return [], False
+        try: 
+            bbox_norm = self.df_json['detections'][self.k][0]["bbox"]
+        except:
+            bbox_norm = []    
+        self.k += 1
+        if bbox_norm != []:
+            img_h, img_w = image.shape[:2]
+            xmin = int(bbox_norm[0] * img_w)
+            ymin = int(bbox_norm[1] * img_h)
+            box_w = int(bbox_norm[2] * img_w)
+            box_h = int(bbox_norm[3] * img_h)
+            box_size = max(box_w, box_h)
+            xmin = max(0, min(xmin - int((box_size - box_w) / 2),img_w - box_w))
+            ymin = max(0, min(ymin - int((box_size - box_h) / 2),img_h - box_h))
+            box_w = min(img_w, box_size)
+            box_h = min(img_h, box_size)
+            croppedimage = image[max(0,ymin):min(img_h,ymin + box_h),
+                                 max(0,xmin):min(img_w,xmin + box_w)]
+
+            return croppedimage, True
+        else:
+            return [], False
+        
