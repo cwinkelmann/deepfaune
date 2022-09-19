3876565477ccf3e731221bb0677f844d4b8c4753
PNRIA - Julien <julien.rabault@irit.fr>
2022-09-19 17:15:04 +0200

    Move model in classif  + clean

--- classifTools.py
+++ classifTools.py
@@ -31,49 +31,127 @@
 # The fact that you are presently reading this means that you have had
 # knowledge of the CeCILL license and that you accept its terms.
 
+import sys
 ####################################################################################
 ### LOADING CLASSIFIER
 ####################################################################################
 from os import environ
 
+import numpy as np
+import timm
+import torch
+import torch.nn as nn
 from PIL import Image
 from torchvision.transforms import InterpolationMode, transforms
 
-from devtest.model import Model
-
-environ['TF_CPP_MIN_LOG_LEVEL'] = '3' # silencing TensorFlow
-from cv2 import cvtColor, COLOR_BGR2RGB, resize, cv2
+from cv2 import cv2
 
 CROP_SIZE = 300
 NBCLASSE = 22
 BACKBONE = "efficientnet_b3"
 weight_path = "efficientnet_b3_22_produc3.pt"
 
-txt_classes = {'fr':["blaireau","bouquetin","cerf","chamois","chat","chevreuil","chien","ecureuil","equide","lagomorphe","loup","lynx","marmotte","micromammifere","mouflon","mouton","mustelide" ,"oiseau","ours","renard","sanglier","vache"]  ,
-              'gb':["badger", "ibex", "deer", "chamois", "cat", "roe_deer", "dog", "squirrel", "equid", "lagomorph", "wolf", "lynx", "marmot", "small_mammal", "mouflon", "sheep", "mustelid" ",bird", "bear", "fox", "wild_boar", "cow"]}
+txt_classes = {
+    'fr': ["blaireau", "bouquetin", "cerf", "chamois", "chat", "chevreuil", "chien", "ecureuil", "equide", "lagomorphe",
+           "loup", "lynx", "marmotte", "micromammifere", "mouflon", "mouton", "mustelide", "oiseau", "ours", "renard",
+           "sanglier", "vache"],
+    'gb': ["badger", "ibex", "deer", "chamois", "cat", "roe deer", "dog", "squirrel", "equid", "lagomorph", "wolf",
+           "lynx", "marmot", "micromammal", "mouflon", "sheep", "mustelid" ",bird", "bear", "fox", "wild boar", "cow"]}
 
-    
+
 ####################################################################################
-### CLASSIFIER 
+### CLASSIFIER
 ####################################################################################
 class Classifier:
-    
+
     def __init__(self):
         self.model = Model(backbone=BACKBONE, num_classes=NBCLASSE)
-        self.model.load_weights(weight_path)
+        self.model.loadWeights(weight_path)
         self.transforms = transforms.Compose(
     [transforms.Resize((300, 300), interpolation=InterpolationMode.NEAREST),
          transforms.ToTensor()])
-        
+
     def predictOnBatch(self, batchtensor, workers=1):
         return self.model.predict(batchtensor)
 
     # croppedimage in BGR loaded by opencv
     def preprocessImage(self, croppedimage):
         croppedimage = cv2.cvtColor(croppedimage, cv2.COLOR_BGR2RGB)
-        croppedimage_pil = Image.fromarray(croppedimage)
-        batch = self.transforms(croppedimage_pil)
-        #gestion plusieurs images
-        if len(batch.shape) == 3:
-            batch = batch.unsqueeze(dim=0)
+        croppedimagePil = Image.fromarray(croppedimage)
+        batch = self.transforms(croppedimagePil)
+        batch = batch.unsqueeze(dim=0)
         return batch
+
+
+####################################################################################
+### MODEL
+####################################################################################
+
+class Model(nn.Module):
+    def __init__(self, backbone="efficientnet_b3", num_classes=10):
+        """
+        Constructor of model using pre-train image detector with classifier
+
+        :param backbone: name of pre-train model (see >>>timm.list_models(pretrained=True))  : str
+        :param num_classes: number of class for classification : int
+        """
+        super().__init__()
+        if backbone not in timm.list_models(pretrained=True):
+            raise Exception("{} is not a known pretrain model \n Please choose a model in this list :"
+                            "({})".format(backbone, timm.list_models(pretrained=True)))
+        self.base_model = timm.create_model(backbone, pretrained=True, num_classes=num_classes)
+        self.backbone = backbone
+        self.num_classes = num_classes
+
+    def forward(self, input):
+        x = self.base_model(input)
+        return x
+
+    def predict(self, data):
+        """
+        Predict on test DataLoader
+        :param test_loader: test dataloader: torch.utils.data.DataLoader
+        :return: numpy array of predictions without soft max
+        """
+        self.eval()
+        device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+        total_output = []
+        with torch.no_grad():
+            x = data.to(device)
+            output = self.forward(x).softmax(dim=1)
+            total_output += output.tolist()
+
+        return np.array(total_output)
+
+    def loadWeights(self, path):
+        """
+        :param path: path of .pt save of model
+        """
+        device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+        if path[-3:] != ".pt":
+            path += ".pt"
+
+        print("#" * 20)
+        print("\n Loading...")
+        try:
+            params = torch.load(path, map_location=device)
+            args = params['args']
+            if self.backbone != args['backbone']:
+                raise Exception("You load a model ({}) that does not have the same architecture as the initial model "
+                                "({})".format(args['backbone'], self.backbone))
+            if self.num_classes != args['num_classes']:
+                raise Exception("You load a model ({}) that does not have the same number of class"
+                                "({})".format(args['num_classes'], self.num_classes))
+
+            self.backbone = args['backbone']
+            self.num_classes = args['num_classes']
+            self.load_state_dict(params['state_dict'])
+            print("\n The loading checkpoint was successful ! \n")
+            print("\tModel : ", self.backbone)
+            print("\tNumber of classes : ", self.num_classes)
+            print("")
+        except Exception as e:
+            print("\n/!\ Can't load checkpoint model /!\ because :\n\n " + str(e), file=sys.stderr)
+            raise e
+        print("#" * 20)
--- devtest/testDetectTools.py
+++ devtest/testDetectTools.py
@@ -1,16 +1,15 @@
 import sys
 import os
-curdir = os.path.abspath(os.path.dirname(sys.argv[0])) 
+
+import torch
+
+curdir = os.path.abspath(os.path.dirname(sys.argv[0]))
 sys.path.append(curdir+'/../')
 
 #######################
 #######################
 import cv2
-#image = cv2.imread(curdir+"/chamois.jpg")
-#image = cv2.imread('/home/vmiele/Developpement/deepfaunegui-modular/testdata/sanglier.jpg')
-#image = cv2.imread('/home/vmiele/Developpement/deepfaunegui-modular/img.jpg')
-#image = cv2.imread('/home/vmiele/Developpement/deepfaunegui/bug2/img0.jpg')
-image = cv2.imread('/home/vmiele/Developpement/deepfaunegui/outofsample_data/chevreuil_ofb_gaudry_IMG_0238.JPG')
+image = cv2.imread('testdata/cerf_neige.jpg')
 #image = cv2.resize(image, (1200,800))
 threshold = 0.25
 croppedimage = image
@@ -22,21 +21,9 @@ detector = Detector()
 croppedimage, nonempty = detector.bestBoxDetection(image, 0.25)
 print(nonempty)
 
-
 #######################
 #######################
-from cv2 import cvtColor,COLOR_BGR2RGB,resize
-CROP_SIZE=300
-croppedimage2classifier =  resize(cvtColor(croppedimage, COLOR_BGR2RGB), (CROP_SIZE,CROP_SIZE))
-
-from tensorflow.keras.applications.efficientnet import preprocess_input
-preprocess_input(croppedimage2classifier)
-import tensorflow as tf
-tf.keras.utils.save_img("img.jpg",preprocess_input(croppedimage2classifier))
-
-#######################
-#######################
-from cv2 import cvtColor,COLOR_BGR2RGB,resize
+from cv2 import resize
 CROP_SIZE=300
 cv2.imshow('window', resize(croppedimage, (CROP_SIZE,CROP_SIZE)))
 k = cv2.waitKey(0)
@@ -47,10 +34,9 @@ if k == 27:         # wait for ESC key to exit
 #######################
 #######################
 from classifTools import Classifier, CROP_SIZE
-import numpy as np
 classifier = Classifier()
-cropped_data = np.ones(shape=(1,CROP_SIZE,CROP_SIZE,3), dtype=np.float32)
-cropped_data[0,:,:,:] = preprocess_input(croppedimage2classifier)
+cropped_data = torch.ones((1,3,CROP_SIZE,CROP_SIZE), dtype=torch.float32)
+cropped_data[0,:,:,:] = classifier.preprocessImage(croppedimage)
 pred = classifier.predictOnBatch(cropped_data)
 (pred*100).astype("int")
 
