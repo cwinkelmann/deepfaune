4408a9d631973ea3c57d0cd039c9b788407a85ae
PNRIA - Julien <julien.rabault@irit.fr>
2023-01-16 16:47:07 +0100

    remove print

--- deepfauneGUI.py
+++ deepfauneGUI.py
@@ -135,7 +135,6 @@ def frgbprint(txt_fr, txt_gb, end='\n'):
 ####################################################################################
 
 def draw_boxes(imagecv,box):
-    print(imagecv.shape)
     cv2.rectangle(imagecv, (int(box[0]), int(box[1])), (int(box[2]), int(box[3])), (0, 0, 255), imagecv.shape[0]//100)
         
 ####################################################################################
