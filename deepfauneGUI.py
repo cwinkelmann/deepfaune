35a69a443a60248c18ca55bb5ab4255e83687f0f
Vincent Miele <vincent.miele@univ-lyon1.fr>
2023-05-23 16:12:32 +0200

    Deutsch translation without species names

--- deepfauneGUI.py
+++ deepfauneGUI.py
@@ -46,7 +46,7 @@ VERSION = "1.0.0"
 ####################################################################################
 ### PARAMETERS
 ####################################################################################
-listlang = ['fr', 'en', 'it']
+listlang = ['fr', 'en', 'it', 'de']
 import configparser
 config = configparser.ConfigParser()
 config.read('settings.ini')
@@ -66,35 +66,54 @@ maxlag = maxlag_default = 10 # seconds
 ### GUI TEXT
 ####################################################################################
 from predictTools import txt_undefined, txt_empty, txt_classes
-txt_other =  {'fr':"autre", 'en':"other", 'it':"altro"}
-txt_browse = {'fr':"Choisir", 'en':"Select", 'it':"Scegliere"}
-txt_incorrect = {'fr':"Dossier incorrect - aucun media trouvé", 'en':"Incorrect folder - no media found", 'it':"File scorretto - media non trovato"}
-txt_confidence = {'fr':"Seuil de confiance", 'en':"Confidence threshold", 'it':"Livello minimo di affidabilita"}
-txt_sequencemaxlag = {'fr':"Intervalle max / séquence (secondes)", 'en':"Sequence max lag (seconds)", 'it':"Intervallo massimo / sequenza (secondi)"}
-txt_configrun = {'fr':"Configurer et lancer", 'en':"Configure & Run", 'it':"Configurare e inviare"}
-txt_run = {'fr':"Lancer", 'en':"Run", 'it':"Inviare"}
-txt_nextpred = {'fr':"Suivant", 'en':"Next", 'it':"Prossimo"}
-txt_prevpred = {'fr':"Précédent", 'en':"Previous", 'it':"Precedente"}
-txt_paramframe = {'fr':"Paramètres", 'en':"Parameters", 'it':"Parametri"}
-txt_selectclasses = {'fr':"Sélection des classes", 'en':"Classes selection", 'it':"Selezione delle classi"}
-txt_close  = {'fr':"Fermer", 'en':"Close", 'it':"Chiudere"}
-txt_all = {'fr':"toutes", 'en':"all", 'it':"tutte"}
-txt_classnotfound = {'fr':"Aucun média pour cette classe", 'en':"No media found for this class", 'it':"Nessun media per questa classe"}
-txt_filename = {'fr':"Nom de fichier", 'en':"Filename", 'it':"Nome del file"}
-txt_prediction = {'fr':"Prédiction", 'en':"Prediction", 'it':"Predizione"}
-txt_count = {'fr':"Comptage", 'en':"Count", 'it':"Conto"}
-txt_counttype = {'fr':"Type de comptage (expérimental)", 'en':"Type of count (experimental)", 'it':"Tipo di conto (sperimentale)"}
-txt_seqnum = {'fr':"Numéro de séquence", 'en':"Sequence ID", 'it':"Sequenza ID"}
-txt_error = {'fr':"Erreur", 'en':"Error", 'it':"Errore"}
+txt_other =  {'fr':"autre", 'en':"other",
+              'it':"altro", 'de':"andere Klasse"}
+txt_browse = {'fr':"Choisir", 'en':"Select",
+              'it':"Scegliere", 'de':"Wählen"}
+txt_incorrect = {'fr':"Dossier incorrect - aucun media trouvé", 'en':"Incorrect folder - no media found",
+                 'it':"File scorretto - media non trovato", 'de':"Falscher Ordner - keine Medien gefunden"}
+txt_confidence = {'fr':"Seuil de confiance", 'en':"Confidence threshold",
+                  'it':"Livello minimo di affidabilita", 'de':"Konfidenzniveau"}
+txt_sequencemaxlag = {'fr':"Intervalle max par séquence (secondes)", 'en':"Max sequence lag (seconds)",
+                      'it':"Intervallo massimo per sequenza (secondi)", 'de':"Maximale Sequenz Zeitintervall (Sekunden)"}
+txt_configrun = {'fr':"Configurer et lancer", 'en':"Configure & Run",
+                 'it':"Configurare e inviare", 'de':"Konfigurieren und starten"}
+txt_run = {'fr':"Lancer", 'en':"Run",
+           'it':"Inviare", 'de':"Starten"}
+txt_next = {'fr':"Suivant", 'en':"Next",
+            'it':"Prossimo", 'de':"Nächstes"}
+txt_prev = {'fr':"Précédent", 'en':"Previous",
+            'it':"Precedente", 'de':"Vorheriges"}
+txt_paramframe = {'fr':"Paramètres", 'en':"Parameters",
+                  'it':"Parametri", 'de':"Parameter"}
+txt_selectclasses = {'fr':"Sélection des classes", 'en':"Classes selection",
+                     'it':"Selezione delle classi", 'de':"Auswahl der Klassen"}
+txt_all = {'fr':"toutes", 'en':"all",
+           'it':"tutte", 'de':"alles"}
+txt_classnotfound = {'fr':"Aucun média pour cette classe", 'en':"No media found for this class",
+                     'it':"Nessun media per questa classe", 'de':"Keine Medien für diese Klasse gefunden"}
+txt_filename = {'fr':"Nom de fichier", 'en':"Filename",
+                'it':"Nome del file", 'de':"Dateiname"}
+txt_prediction = {'fr':"Prédiction", 'en':"Prediction",
+                  'it':"Predizione", 'de':"Vorhersage"}
+txt_count = {'fr':"Comptage", 'en':"Count",
+             'it':"Conto", 'de':"Zählung"}
+txt_seqnum = {'fr':"Numéro de séquence", 'en':"Sequence ID",
+              'it':"Sequenza ID", 'de':"Sequenz ID"}
+txt_error = {'fr':"Erreur", 'en':"Error",
+             'it':"Errore", 'de':"Fehler"}
 txt_savepredictions = {'fr':"Voulez-vous enregistrer les prédictions dans ", 'en':"Do you want to save predictions in ",
-                       'it':"Volete registrare le predizioni nel"}
-txt_destcopy = {'fr':"Copier dans des sous-dossiers de :", 'en':"Copy in subfolders of:", 'it':"Copiare nei sotto file di"}
-txt_destmove = {'fr':"Déplacer vers des sous-dossiers de :", 'en':"Move to subfolders of:", 'it':"Spostare nei sotto file di"}
+                       'it':"Volete registrare le predizioni nel ", 'de':"Möchten Sie Vorhersagen speichern"}
+txt_destcopy = {'fr':"Copier dans des sous-dossiers de", 'en':"Copy in subfolders of",
+                'it':"Copiare nei sotto file di", 'de':"In Unterordner Kopieren"}
+txt_destmove = {'fr':"Déplacer vers des sous-dossiers de", 'en':"Move to subfolders of",
+                'it':"Spostare nei sotto file di", 'de':"In Unterordner Verschieben"}
 txt_loadingmetadata = {'fr':"Chargement des metadonnées... (cela peut prendre du temps)", 'en':"Loading metadata... (this may take a while)",
-                       'it':"Carica dei metadata... (puo essere lungo)"}
+                       'it':"Carica dei metadata... (puo essere lungo)", 'de':"Laden der Metadaten... (dies kann eine Weile dauern)"}
 txt_restart = {'fr':"Redémarrage nécessaire pour changer la langue. Arréter le logiciel ?",
                'en':"Restart required to change the language. Stopping the software?",
-               'it':"Per cambiare la lingua è necessario un riavvio. Arresto del software ?"}
+               'it':"Per cambiare la lingua è necessario un riavvio. Arresto del software ?",
+               'de':"Neustart erforderlich, um die Sprache zu ändern. Wollen Sie die Software stoppen?"}
 
 ####################################################################################
 ### THEME SETTINGS
@@ -249,22 +268,38 @@ select_frame = sg.Frame(txt_selectclasses[LANG], listCB, font=FONT_NORMAL, expan
                         background_color=background_color) # required here to avoid element reuse (not accepted) 
 
 # Main window
-txt_file = {'fr':"Fichier", 'en':"File", 'it':"File"}
-txt_pref = {'fr':"Préférences", 'en':"Preferences", 'it':"Preferenze"}
-txt_help = {'fr':"Aide", 'en':"Help", 'it':"Aiuto"}
-txt_import = {'fr':"Importer", 'en':"Import", 'it':"Caricare"}
-txt_importimage = {'fr':"Images", 'en':"Images", 'it':"Immagine"}
-txt_importvideo = {'fr':"Vidéos", 'en':"Videos", 'it':"Video"}
-txt_export = {'fr':"Exporter les résultats", 'en':"Export results", 'it':"Esportare i risultati"}
-txt_ascsv = {'fr':"Format CSV", 'en':"As CSV", 'it':"Formato CSV"}
-txt_asxlsx = {'fr':"Format XSLX", 'en':"As XSLX", 'it':"Formato XSLX"}
-txt_createsubfolders = {'fr':"Créer des sous-dossiers", 'en':"Create subfolders", 'it':"Creare dei sotto file"}
-txt_copy = {'fr':"Copier les fichiers", 'en':"Copy files", 'it':"Copiare i file"}
-txt_move = {'fr':"Déplacer les fichiers", 'en':"Move files", 'it':"Spostare i file"}
-txt_language = {'fr':"Langue", 'en':"Language", 'it':"Lingua"}
-txt_activatecount = {'fr':"Activer le comptage (expérimental)", 'en':"Activate count (experimental)", 'it':"Attivare il conto (sperimentale)"}
-txt_deactivatecount = {'fr':"Désactiver le comptage (expérimental)", 'en':"Deactivate count (experimental)", 'it':"Disattivare il conto (sperimentale)"}
-txt_credits = {'fr':"A propos", 'en':"About DeepFaune", 'it':"A proposito"}
+txt_file = {'fr':"Fichier", 'en':"File",
+            'it':"File", 'de':"Datei"}
+txt_pref = {'fr':"Préférences", 'en':"Preferences",
+            'it':"Preferenze", 'de':"Präferenzen"}
+txt_help = {'fr':"Aide", 'en':"Help",
+            'it':"Aiuto", 'de':"Hilfe"}
+txt_import = {'fr':"Importer", 'en':"Import",
+              'it':"Caricare", 'de':"Importieren"}
+txt_importimage = {'fr':"Images", 'en':"Images",
+                   'it':"Immagine", 'de':"Bilder"}
+txt_importvideo = {'fr':"Vidéos", 'en':"Videos",
+                   'it':"Video", 'de':"Videos"}
+txt_export = {'fr':"Exporter les résultats", 'en':"Export results",
+              'it':"Esportare i risultati", 'de':"Resultate exportieren"}
+txt_ascsv = {'fr':"Format CSV", 'en':"As CSV",
+             'it':"Formato CSV", 'de':"Als CSV"}
+txt_asxlsx = {'fr':"Format XSLX", 'en':"As XSLX",
+              'it':"Formato XSLX", 'de':"Als XLSX"}
+txt_createsubfolders = {'fr':"Créer des sous-dossiers", 'en':"Create subfolders",
+                        'it':"Creare dei sotto file", 'de':"Unterordner erstellen"}
+txt_copy = {'fr':"Copier les fichiers", 'en':"Copy files",
+            'it':"Copiare i file", 'de':"Dateien kopieren"}
+txt_move = {'fr':"Déplacer les fichiers", 'en':"Move files",
+            'it':"Spostare i file", 'de':"Dateien verschieben"}
+txt_language = {'fr':"Langue", 'en':"Language",
+                'it':"Lingua", 'de':"Sprache"}
+txt_activatecount = {'fr':"Activer le comptage (expérimental)", 'en':"Activate count (experimental)",
+                     'it':"Attivare il conto (sperimentale)", 'de':"Zählung aktivieren (experimentell)"}
+txt_deactivatecount = {'fr':"Désactiver le comptage (expérimental)", 'en':"Deactivate count (experimental)",
+                       'it':"Disattivare il conto (sperimentale)", 'de':"Zählung desaktivieren (experimentell)"}
+txt_credits = {'fr':"A propos", 'en':"About DeepFaune",
+               'it':"A proposito", 'de':"Über DeepFaune"}
 if countactivated:
     txt_statuscount = txt_deactivatecount[LANG]
 else:
@@ -304,8 +339,8 @@ layout = [
                         sg.Combo(values=[txt_all[LANG]]+sorted_txt_classes_lang+[txt_undefined[LANG],txt_empty[LANG]],
                                  background_color=background_color, text_color=text_color, enable_events=True,
                                  default_value=txt_all[LANG], size=(12, 1), bind_return_key=False, key='-RESTRICT-'),
-                        sg.Button(key='-PREVIOUS-', image_data=PREVIOUS_BUTTON_IMG, button_color=(background_color,background_color), tooltip='previous media'),
-                        sg.Button(key='-NEXT-', image_data=NEXT_BUTTON_IMG, button_color=(background_color,background_color), tooltip='next media')
+                        sg.Button(key='-PREVIOUS-', image_data=PREVIOUS_BUTTON_IMG, button_color=(background_color,background_color), tooltip=None),
+                        sg.Button(key='-NEXT-', image_data=NEXT_BUTTON_IMG, button_color=(background_color,background_color), tooltip=None)
                      ]
                 ], background_color=background_color, expand_y=True),
                 sg.Column([ 
