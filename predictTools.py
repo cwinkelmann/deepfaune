Correct predictionscore_base with softmax. STill a problem with score=1. for human/vehicle not being a logit

predictTools.py
@@ -141,18 +141,6 @@ class PredictorBase(ABC):
         else:            
             self.predictedscore += predictor.predictedscore
         self.resetBatch()        
-      
-    def __score2class(self, pred):
-        txt_classesempty_lang = txt_classes[self.LANG] + [txt_empty[self.LANG]]
-        if len(self.idxforbidden):
-            pred[self.idxforbidden] = 0.
-            if np.sum(pred)>0: # possibly=0 due to forbidden classes
-                pred = pred/np.sum(pred)
-        idxmax = np.argmax(pred)
-        if max(pred)>self.threshold:
-            return txt_classesempty_lang[idxmax], int(max(pred)*100)/100.
-        else:
-            return txt_undefined[self.LANG], int(max(pred)*100)/100.  
         
     def __averageLogitInSequence(self, predinseq):
         notempty = ((predinseq[:,-1]==1.)!=True)
@@ -186,7 +174,7 @@ class PredictorImageBase(PredictorBase):
             predictedclass_base = [""]*self.fileManager.nbFiles()
             predictedscore_base = [0.]*self.fileManager.nbFiles()
             for k in range(0,self.fileManager.nbFiles()):
-                predictedclass_base[k], predictedscore_base[k] = self._PredictorBase__score2class(self.prediction[k,])   
+                predictedclass_base[k], predictedscore_base[k] = self._PredictorBase__averageLogitInSequence(self.prediction[k:(k+1),])   
             return predictedclass_base, predictedscore_base, self.bestboxes, self.count
 
     def setPredictedClassInSequence(self, k, label, score=1.0):