6605b7481b9726052de435c78d05158d50591aed
Vincent Miele <vincent.miele@univ-lyon1.fr>
2024-01-15 21:21:51 +0100

    Datetime format fixed

--- fileManager.py
+++ fileManager.py
@@ -36,6 +36,7 @@ from PIL import Image
 from datetime import datetime, timedelta
 import os.path as op
 import ffmpeg
+from datetime import datetime
 
 def getFilesOrder(filenames):
     nbfiles = len(filenames)
@@ -66,7 +67,8 @@ def getDateFromMetadata(filename):
     # Video file
     try:
         # works for MOV/MP4/MKV files
-        date = ffmpeg.probe(filename)["streams"][0]['tags']['creation_time']
+        creation_time = ffmpeg.probe(filename)["streams"][0]['tags']['creation_time']
+        date = str(datetime.strptime(creation_time.replace(".000000Z", ""), '%Y-%m-%dT%H:%M:%S'))
     except: # KeyError
         pass
     return date
